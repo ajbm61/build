3fb060098ba9784c86236af28d44d44869749ace
crynobone <crynobone@gmail.com>
2013-10-19 12:18:45 +0800

    Include export platform docs
    Signed-off-by: crynobone <crynobone@gmail.com>

--- build-docs.sh
+++ build-docs.sh
@@ -1,13 +1,27 @@
 if [ "$1" == "master" ]; then TARGET="2.1"; else TARGET=$1; fi
 BRANCH=$1;
 
+function build_platform {
+    cd ../platform;
+    echo '-------';
+    pwd;
+    echo "git checkout $BRANCH";
+    git checkout $BRANCH;
+    rm -Rf ../docs/$TARGET/*;
+    cp -r docs/* ../docs/$TARGET/;
+    mv ../docs/$TARGET/readme.md ../docs/$TARGET/index.md;
+    mkdir ../docs/$TARGET/components;
+}
+
 function build_docs {
     echo "git checkout $BRANCH";
     git checkout $BRANCH;
-    rm -Rf ../docs/$TARGET/components/$DIR/*;
+    mkdir ../docs/$TARGET/components/$DIR;
     cp -r docs/* ../docs/$TARGET/components/$DIR/;
     mv ../docs/$TARGET/components/$DIR/readme.md ../docs/$TARGET/components/$DIR/index.md;
 }
+
+build_platform;
 
 for DIR in asset auth extension facile foundation html memory model resources support translation testbench view widget
 do
@@ -15,5 +29,5 @@ do
     cd $DIR;
     echo '-------';
     pwd;
-    build_docs
+    build_docs;
 done
