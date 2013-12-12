729346482e6177f7c999f9f425cab38ec50b424d
crynobone <crynobone@gmail.com>
2013-12-12 09:14:17 +0800

    Create new branch from master script
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ create-branch.sh
@@ -0,0 +1,17 @@
+CREATE=$1;
+
+function create_branch {
+    echo "git checkout master";
+    git checkout master;
+    echo "git checkout -b $CREATE";
+    git checkout -b $CREATE;
+}
+
+for DIR in asset auth debug extension facile foundation html imagine memory model optimize resources support translation testbench view widget platform
+do
+    cd ../;
+    cd $DIR;
+    echo '-------';
+    pwd;
+    create_branch
+done
