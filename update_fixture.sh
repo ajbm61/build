bc995fe220e46b667a7a7fdd6163adf3416f5e55
crynobone <crynobone@gmail.com>
2014-12-17 22:41:15 +0800

    Rename and update fixture.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- update_fixture.sh
+++ update_fixture.sh
@@ -3,11 +3,11 @@ BRANCH=master;
 cd ../platform
 git checkout $BRANCH;
 
-cd ../foundation
+cd ../testing
 git checkout $BRANCH;
 
 for DIR in config lang views 
 do
-    cp -Rf ../platform/resources/$DIR/* src/Testing/fixture/resources/$DIR
+    cp -Rf ../platform/resources/$DIR/* fixture/resources/$DIR
     echo "COPIED $DIR";
 done
